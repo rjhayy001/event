e81a30949ca9da008170c72b4d380ffab20776bf
rjhayy <reitheljay25@gmail.com>
2020-02-12 17:27:53 +0800

    login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,6 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
-        <link rel="icon" href="http://event-app.ohm-conception.com/Settings/logo.png" type="image/gif" sizes="16x16">
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
         integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
         crossorigin=""/>
