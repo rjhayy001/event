dfdf9792ba3558c224708f11aa27566535434ef7
rjhayy <reitheljay25@gmail.com>
2020-03-02 17:21:44 +0800

    test

--- app/Http/Controllers/Backend/VisitorController.php
+++ app/Http/Controllers/Backend/VisitorController.php
@@ -73,6 +73,9 @@ class VisitorController extends Controller
         if($visitor->save() && $visitor->fcmtoken != null ) {
             $this->send_deactivate_notification_android($visitor);
         }
+        if($visitor->save() && $visitor->iostoken != null ){
+            $this->send_deactivate_notification_ios($visitor);
+        }
         return $this->index() ;
     }
 
@@ -113,6 +116,36 @@ class VisitorController extends Controller
         $result = json_decode($result,true);
         $responseData['android'] =[ "result" =>$result ];
         curl_close( $ch );
+    }
+    public function send_deactivate_notification_ios($visitor) {
+        $tokens[] = $visitor->iostoken ;
+        $url = "https://fcm.googleapis.com/fcm/send";
+        $serverKey = 'AAAAf9lPnKM:APA91bH035-_L3lCbPRbasyUKz7LqRUMe5KEevmgnn843nPY71O4pSnmvO4Y5UuoVLTuVEBPpSbSzF7Ds-tuYSCHLecqhgG1VG_KcNioyQZ7yLz2g95Ide7z3UBXRFIP0ASJYfPgv9dp';
+        $title = $visitor->active == 0 ? 'Deactivation Notice' : 'Activation Notice';
+        $body = $visitor->active == 0 ? 'youre account has been deactivated !' : 'youre account has been activated !';
+        $notification = array('title' =>$title , 'text' => $body, 'sound' => 'default', 'badge' => '3' );
+        $arrayToSend = array('registration_ids' => $tokens, 'notification' => $notification,'priority'=>'high');
+        $json = json_encode($arrayToSend);
+        $headers = array();
+        $headers[] = 'Content-Type: application/json';
+        $headers[] = 'Authorization: key='. $serverKey;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST,"POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+        curl_setopt($ch, CURLOPT_HTTPHEADER,$headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        //Send the request
+        $result = curl_exec($ch);
+        if ($result === FALSE) 
+        {
+                die('FCM Send Error: ' . curl_error($ch));
+        }
+        $result = json_decode($result,true);
+        $responseData['ios'] =[
+            "result" =>$result
+        ];
+        curl_close($ch);
     }
 
     /**
